877d75d80e8fbe0caf05430a9a188f728f4edff0
shaham7 <mohd.shaham7@gmail.com>
2024-12-07 11:02:44 +0400

    git push origin main

--- PasswordChecker/PassChecker.py
+++ PasswordChecker/PassChecker.py
@@ -1,6 +1,13 @@
 import requests
 
-url = 'https://api.pwnedpasswords.com/range/'+'CBFDA'
-res = requests.get(url)
+def request_api_data(query_char): 
+    url = 'http://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try agian')
 
-print(res)
+def pwned_api_check(password): 
+    #check pass if it exists in API response
+    pass
+
+request_api_data('CBFDA')
