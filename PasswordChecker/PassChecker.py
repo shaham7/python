6ea06b0fc51291e5ec8b8f0f33fdb77a117ffa5b
shaham7 <mohd.shaham7@gmail.com>
2024-12-07 11:42:50 +0400

    Testing

--- PasswordChecker/PassChecker.py
+++ PasswordChecker/PassChecker.py
@@ -1,13 +1,17 @@
 import requests
+import hashlib
 
 def request_api_data(query_char): 
-    url = 'http://api.pwnedpasswords.com/range/' + query_char
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
+    print(res.status_code)
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try agian')
 
-def pwned_api_check(password): 
-    #check pass if it exists in API response
-    pass
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(
+        password.encode('utf-8')
+    ).hexdigest().upper()
+    print(sha1password)
 
-request_api_data('CBFDA')
+pwned_api_check('password123')
