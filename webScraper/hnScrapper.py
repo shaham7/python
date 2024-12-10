bf6c5b31f79e83c06b1616910c561b6ba5217b58
shaham7 <mohd.shaham7@gmail.com>
2024-12-10 09:06:52 +0400

    web scraping project completed

--- webScraper/hnScrapper.py
+++ webScraper/hnScrapper.py
@@ -3,24 +3,34 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 
 links = soup.select('.titleline > a')
-subtexts = soup.select('.subtext')
+subtext = soup.select('.subtext')
+links2 = soup2.select('.titleline > a')
+subtext2 = soup2.select('.subtext')
+
+comp_links = links + links2
+comp_subtext = subtext + subtext2
+
 
 def sort_by_votes(hnlist):
-    return sorted(hnlist, key= lambda k:k['votes'])
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
-def create_custom_hm(link, subtext):
+
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
-        title = link[idx].getText()
-        href = link[idx].get('href',None)
+        title = item.getText()
+        href = item.get('href', None)
         vote = subtext[idx].select('.score')
         if len(vote):
-            points = int(vote[0].getText().replace(' points',''))
+            points = int(vote[0].getText().replace(' points', ''))
             if points > 99:
-                hn.append({'title':title, 'link':href, 'votes':points})
+                hn.append({'title': title, 'link': href, 'votes': points})
     return sort_by_votes(hn)
-#more mode left
-pprint.pprint(create_custom_hm(links,subtexts))
+
+
+pprint.pprint(sort_by_votes(create_custom_hn(comp_links, comp_subtext)))
