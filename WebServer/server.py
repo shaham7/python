cc28e8be7f5dc0cf702d114089ce7e8f72093fda
shaham7 <mohd.shaham7@gmail.com>
2024-12-10 20:38:42 +0400

    webserver

---
+++ WebServer/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id=post_id)
+
+@app.route("/about")
+def about():
+    return render_template('./about.html')
