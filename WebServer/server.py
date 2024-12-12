3f26b9e279500237cb33495e20343a49365f1554
shaham7 <mohd.shaham7@gmail.com>
2024-12-12 10:04:53 +0400

    ending this here, too boring

--- WebServer/server.py
+++ WebServer/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +8,11 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_from():
+    if request.method == 'POST':
+        data = request.form.to.dict()
+        print(data)
+        return 'form submitted'
+    return '??'
